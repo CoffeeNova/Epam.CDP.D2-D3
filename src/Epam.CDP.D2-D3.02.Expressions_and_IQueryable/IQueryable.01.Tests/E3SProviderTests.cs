a80894576dcea4dee2f4de95324bc013862d0b40
Igor Salzhenitsin <igor.sal@protonmail.com>
2018-03-29 00:24:27 +0300

    module2 task3 2 of 3 points of bulletlist completed

--- src/Epam.CDP.D2-D3.02.Expressions_and_IQueryable/IQueryable.01.Tests/E3SProviderTests.cs
+++ src/Epam.CDP.D2-D3.02.Expressions_and_IQueryable/IQueryable.01.Tests/E3SProviderTests.cs
@@ -10,6 +10,14 @@ namespace IQueryable._01.Tests
     [TestClass]
     public class E3SProviderTests
     {
+        private const string User = nameof(User);
+        private const string Password = nameof(Password);
+
+        [TestInitialize]
+        public void Init()
+        {
+        }
+
         [TestMethod]
         public void WithoutProvider()
         {
@@ -38,6 +46,96 @@ namespace IQueryable._01.Tests
 
             foreach (var emp in employees.Where(e => e.Workstation == "EPRUIZHW0249"))
                 Console.WriteLine($"{emp.NativeName} {emp.StartWorkDate}");
+        }
+
+        [TestMethod]
+        public void E3SEntitySet_Where_Field_Equals_Const()
+        {
+            //Arrange
+            var employees = new E3SEntitySet<EmployeeEntity>(User, Password);
+            var query = employees.Where(e => e.Workstation == "EPRUIZHW0249");
+
+            //Act
+            var employee = query.FirstOrDefault();
+
+            //Assert
+            Assert.AreEqual("", employee?.NativeName);
+            Assert.AreEqual("", employee?.StartWorkDate);
+        }
+
+        [TestMethod]
+        public void E3SEntitySet_Where_Const_Equals_Field()
+        {
+            //Arrange
+            var employees = new E3SEntitySet<EmployeeEntity>(User, Password);
+            var query = employees.Where(e => "EPRUIZHW0249" == e.Workstation);
+
+            //Act
+            var employee = query.FirstOrDefault();
+
+            //Assert
+            Assert.AreEqual("", employee?.NativeName);
+            Assert.AreEqual("", employee?.StartWorkDate);
+        }
+
+        [TestMethod]
+        public void E3SEntitySet_Where_StartsWith()
+        {
+            //Arrange
+            var employees = new E3SEntitySet<EmployeeEntity>(User, Password);
+            var query = employees.Where(e => e.Workstation.StartsWith("EPRUIZHW006"));
+
+            //Act
+            var employee = query.FirstOrDefault();
+
+            //Assert
+            Assert.AreEqual("", employee?.NativeName);
+            Assert.AreEqual("", employee?.StartWorkDate);
+        }
+
+        [TestMethod]
+        public void E3SEntitySet_Where_EndsWith()
+        {
+            //Arrange
+            var employees = new E3SEntitySet<EmployeeEntity>(User, Password);
+            var query = employees.Where(e => e.Workstation.EndsWith("IZHW0060"));
+
+            //Act
+            var employee = query.FirstOrDefault();
+
+            //Assert
+            Assert.AreEqual("", employee?.NativeName);
+            Assert.AreEqual("", employee?.StartWorkDate);
+        }
+
+        [TestMethod]
+        public void E3SEntitySet_Where_Contains()
+        {
+            //Arrange
+            var employees = new E3SEntitySet<EmployeeEntity>(User, Password);
+            var query = employees.Where(e => e.Workstation.Contains("IZHW006"));
+
+            //Act
+            var employee = query.FirstOrDefault();
+
+            //Assert
+            Assert.AreEqual("", employee?.NativeName);
+            Assert.AreEqual("", employee?.StartWorkDate);
+        }
+
+        [TestMethod]
+        public void E3SEntitySet_And()
+        {
+            //Arrange
+            var employees = new E3SEntitySet<EmployeeEntity>(User, Password);
+            var query = employees.Where(e => e.Workstation.Contains("IZHW006"));
+
+            //Act
+            var employee = query.FirstOrDefault();
+
+            //Assert
+            Assert.AreEqual("", employee?.NativeName);
+            Assert.AreEqual("", employee?.StartWorkDate);
         }
     }
 }
--- src/Epam.CDP.D2-D3.02.Expressions_and_IQueryable/IQueryable.01/ExpressionToFTSRequestTranslator.cs
+++ src/Epam.CDP.D2-D3.02.Expressions_and_IQueryable/IQueryable.01/ExpressionToFTSRequestTranslator.cs
@@ -5,67 +5,96 @@ using System.Text;
 
 namespace IQueryable._01
 {
-	public class ExpressionToFtsRequestTranslator : ExpressionVisitor
-	{
-	    private StringBuilder _resultString;
+    public class ExpressionToFtsRequestTranslator : ExpressionVisitor
+    {
+        private StringBuilder _resultString;
+        private const string Where = nameof(Where);
+        private const string StartsWith = nameof(StartsWith);
+        private const string EndsWith = nameof(EndsWith);
+        private const string Contains = nameof(Contains);
 
-		public string Translate(Expression exp)
-		{
-			_resultString = new StringBuilder();
-			Visit(exp);
+        public string Translate(Expression exp)
+        {
+            _resultString = new StringBuilder();
+            Visit(exp);
 
-			return _resultString.ToString();
-		}
+            return _resultString.ToString();
+        }
 
-		protected override Expression VisitMethodCall(MethodCallExpression node)
-		{
-			if (node.Method.DeclaringType == typeof(Queryable)
-				&& node.Method.Name == "Where")
-			{
-				var predicate = node.Arguments[1];
-				Visit(predicate);
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable) && node.Method.Name == Where)
+            {
+                var predicate = node.Arguments[1];
+                Visit(predicate);
 
-				return node;
-			}
-			return base.VisitMethodCall(node);
-		}
+                return node;
+            }
 
-		protected override Expression VisitBinary(BinaryExpression node)
-		{
-			switch (node.NodeType)
-			{
-				case ExpressionType.Equal:
-					if (node.Left.NodeType != ExpressionType.MemberAccess)
-						throw new NotSupportedException("Left operand should be property or field");
+            //•	Добавить поддержку операций включения (т.е. не точное совпадение со строкой, а частичное). 
+            //  При этом в LINQ-нотации они должны выглядеть как обращение к методам класса string: StartsWith, EndsWith, Contains
+            if (node.Method.DeclaringType == typeof(string))
+            {
+                var argument = (ConstantExpression)node.Arguments[0];
 
-					if (node.Right.NodeType != ExpressionType.Constant)
-						throw new NotSupportedException("Right operand should be constant");
+                if (node.Method.Name == StartsWith)
+                    Visit(Expression.Constant($"{argument.Value }*"));
+                else if (node.Method.Name == EndsWith)
+                    Visit(Expression.Constant($"*{argument.Value }"));
+                else if (node.Method.Name == Contains)
+                    Visit(Expression.Constant($"*{argument.Value }*"));
+            }
 
-					Visit(node.Left);
-					_resultString.Append("(");
-					Visit(node.Right);
-					_resultString.Append(")");
-					break;
+            return base.VisitMethodCall(node);
+        }
 
-				default:
-					throw new NotSupportedException($"Operation {node.NodeType} is not supported");
-			}
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            switch (node.NodeType)
+            {
+                //•	Снять текущее ограничение на порядок операндов выражения.
+                case ExpressionType.Equal:
+                    if (node.Left.NodeType != ExpressionType.MemberAccess && node.Left.NodeType != ExpressionType.Constant)
+                        throw new NotSupportedException("Left operand should be property/field or constant");
 
-			return node;
-		}
+                    if (node.Right.NodeType != ExpressionType.MemberAccess && node.Right.NodeType != ExpressionType.Constant)
+                        throw new NotSupportedException("Right operand should be property/field or constant");
 
-		protected override Expression VisitMember(MemberExpression node)
-		{
-			_resultString.Append(node.Member.Name).Append(":");
+                    if (node.Left.NodeType is ExpressionType.MemberAccess && node.Right.NodeType is ExpressionType.Constant)
+                        VisitAndBuild(node.Left, node.Right);
 
-			return base.VisitMember(node);
-		}
+                    else if (node.Left.NodeType is ExpressionType.Constant && node.Right.NodeType is ExpressionType.MemberAccess)
+                        VisitAndBuild(node.Right, node.Left);
 
-		protected override Expression VisitConstant(ConstantExpression node)
-		{
-			_resultString.Append(node.Value);
+                    break;
 
-			return node;
-		}
-	}
+                default:
+                    throw new NotSupportedException($"Operation {node.NodeType} is not supported");
+            }
+
+            return node;
+        }
+
+        private void VisitAndBuild(Expression exp1, Expression exp2)
+        {
+            Visit(exp1);
+            _resultString.Append("(");
+            Visit(exp2);
+            _resultString.Append(")");
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            _resultString.Append(node.Member.Name).Append(":");
+
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _resultString.Append(node.Value);
+
+            return node;
+        }
+    }
 }
