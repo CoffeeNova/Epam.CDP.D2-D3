08545841b86b46406edbcf4722e9d919e08d7b45
Ihar Salzhanitsyn <ihar_salzhanitsyn@epam.com>
2018-06-06 11:35:18 +0300

    debuggind temp1

---
+++ src/Epam.CDP.D2-D3.07.Debugging/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using CrackMe;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var form1 = new Form1();
+            form1.eval_d.Text = "123";
+            form1.eval_a(null, null);
+
+            var networkInterface = ((IEnumerable<NetworkInterface>)NetworkInterface.GetAllNetworkInterfaces()).FirstOrDefault<NetworkInterface>();
+            var addressBytes = networkInterface?.GetPhysicalAddress().GetAddressBytes();
+            var data = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+
+            var source = addressBytes.Select((x, i) => x ^ data[i]).Select(x =>
+            {
+                if (x <= 999)
+                    return x * 10;
+                return x;
+            });
+            var entry = new[] {123};
+            var res = source.Select((x, i) => x - entry[i]);
+
+        }
+    }
+}
---
+++ src/Epam.CDP.D2-D3.07.Debugging/tempt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Test();
+            t.Method1(1);
+        }
+    }
+
+    class Test
+    {
+        List<int> TEST_A_0;
+
+        public void Method1(int A_0)
+        {
+            if (TEST_A_0 == null)
+                TEST_A_0 = new List<int>();
+
+            TEST_A_0.Add(A_0);
+        }
+
+        public void Method2(int A_0)
+        {
+            Method1(A_0);
+        }
+    }
+}
