9c816f2fdf2d5309695714c2357f09c5e56bc1b3
isalzh <36303904+isalzh@users.noreply.github.com>
2018-06-19 12:05:52 +0300

    Module8 (#8)
    * module8 Profiling_and_Optimization

---
+++ src/Epam.CDP.D2-D3.08.Profiling_and_Optimization/GameOfLife/AdWindowHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace GameOfLife
+{
+    internal static class AdWindowHelper
+    {
+        public static DispatcherTimer CreateAdTimer(EventHandler onTick)
+        {
+            var adTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(3)
+            };
+            adTimer.Tick += onTick;
+            adTimer.Start();
+            return adTimer;
+        }
+
+        public static void UnsubscribeAdTime(this DispatcherTimer adTimer, EventHandler onTick)
+        {
+            if (adTimer == null)
+                throw new ArgumentNullException(nameof(adTimer));
+
+            if (onTick != null)
+                adTimer.Tick -= onTick;
+        }
+    }
+}
