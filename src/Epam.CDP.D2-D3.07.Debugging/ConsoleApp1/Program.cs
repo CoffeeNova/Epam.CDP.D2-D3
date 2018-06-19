b63daa5c1828499b3b2a422ffb9d908e329b11a6
isalzh <36303904+isalzh@users.noreply.github.com>
2018-06-19 16:50:04 +0300

    Module7 (#9)
    * module7 debugging

---
+++ src/Epam.CDP.D2-D3.07.Debugging/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+            var addressBytes = networkInterface?.GetPhysicalAddress().GetAddressBytes();
+            if (addressBytes == null)
+                throw new InvalidOperationException("Please setup atleast one network interface.");
+
+            var dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            var cryptedAddressBytes = addressBytes.Select((x, i) => (x ^ dateBytes[i]) * 10);
+
+            Console.WriteLine($"Correct key: {string.Join("-", cryptedAddressBytes)}");
+            Console.ReadLine();
+        }
+    }
+}
